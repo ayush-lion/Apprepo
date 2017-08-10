b40cb912de41ce83e9cc0cda463685d93da207c8
dell <dell@192.168.1.11>
2017-08-10 01:31:28 -0700

    third

--- JavaApp/src/com/lions/pkg/GitHub2.java
+++ JavaApp/src/com/lions/pkg/GitHub2.java
@@ -3,5 +3,10 @@ package com.lions.pkg;
 public class GitHub2 {
 public static void main(String[] args) {
 	System.out.println("GitHub 2");
+	System.out.println("hello git 1");
+	System.out.println("hello git 2");
+	System.out.println("hello git 3");
+	System.out.println("hello git 4");
+	System.out.println("hello git 5");
 }
 }
